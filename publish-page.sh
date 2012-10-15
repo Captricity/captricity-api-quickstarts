8d0ba468876de3d0cf0c371e80075240877cee0c
Yoriyasu Yano <yoriy@captricity.com>
2012-10-15 11:41:18 -0700

    Resolve namespace error on gh-pages

--- publish-page.sh
+++ publish-page.sh
@@ -4,8 +4,12 @@ git stash
 git checkout gh-pages
 git checkout master javascript/js_widget_example.html
 git reset HEAD javascript/js_widget_example.html
+git checkout master javascript/new_job.js
+git reset HEAD javascript/new_job.js
 mv javascript/js_widget_example.html index.html
+mv javascript/*.js .
 git add index.html
+git add *.js
 git commit -m "Automatic pages update"
 git push origin gh-pages
 git checkout master
